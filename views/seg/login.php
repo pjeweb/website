623375996911f7ca0a24f97e97c52d8b8ea58779
paul <mail@pje-web.de>
2020-10-14 21:53:19 +0200

    login view: use same loop as seg/login.php

--- views/seg/login.php
+++ views/seg/login.php
@@ -1,5 +1,6 @@
 <?php
 namespace Destiny;
+use Destiny\Common\Config;
 use Destiny\Common\Session\Session;
 use Destiny\Common\User\UserRole;
 ?>
@@ -13,21 +14,11 @@ use Destiny\Common\User\UserRole;
                     <input type="hidden" name="grant" value="" />
                     <input type="hidden" name="authProvider" class="hidden" />
                     <div id="loginproviders">
-                        <a class="btn btn-lg btn-twitch" tabindex="1" data-provider="twitch">
-                            <i class="fab fa-twitch"></i> Twitch
-                        </a>
-                        <a class="btn btn-lg btn-google" tabindex="2" data-provider="google">
-                            <i class="fab fa-google"></i> Google
-                        </a>
-                        <a class="btn btn-lg btn-twitter" tabindex="2" data-provider="twitter">
-                            <i class="fab fa-twitter"></i> Twitter
-                        </a>
-                        <a class="btn btn-lg btn-reddit" tabindex="2" data-provider="reddit">
-                            <i class="fab fa-reddit"></i> Reddit
-                        </a>
-                        <a class="btn btn-lg btn-discord" tabindex="2" data-provider="discord">
-                            <i class="fab fa-discord"></i> Discord
-                        </a>
+                        <?php foreach (Config::$a['authProfiles'] as $i => $id): ?>
+                            <a class="btn btn-lg btn-<?=$id?>" tabindex="<?=$i+1?>" data-provider="<?=$id?>">
+                                <i class="fab fa-<?=$id?>"></i> <?=ucwords($id)?>
+                            </a>
+                        <?php endforeach; ?>
                     </div>
                     <div class="form-group form-group-remember-me">
                         <div class="controls checkbox">
