6724f435596bdcd04d93ca7209f5ee32dae33b75
paul <mail@pje-web.de>
2020-10-14 21:49:38 +0200

    chat panel: fix js, add chat panel back after closing popout window

--- assets/web/js/bigsceen/top-panel.js
+++ assets/web/js/bigsceen/top-panel.js
@@ -1,43 +1,63 @@
 import {getPopupOptionsString} from '../common/popups'
 import {applyOrientation, getOrientation, setOrientation} from './layout'
 
-const chatPanelElement = document.getElementById('chat-panel')
+const CHAT_EMBED_URL = '/embed/chat'
+
 const panelToolsElement = document.getElementById('chat-panel-tools')
-const chatIFrameElement = document.querySelector('#chat-wrap iframe')
+let popoutWindow
+
+function hideChat(chatIFrameElement) {
+    document.body.classList.add('nochat')
+    chatIFrameElement.src = '' // Instead of removing the node completely we just unload the iframe content so we can reanimate the chat e.g. if the popout was closed
+}
 
 // Chat top tools
-function onIframeLoaded() {
-    const chatWindow = this.contentWindow
-    if (!chatWindow) {
-        return
+if (panelToolsElement) {
+    const chatIFrameElement = document.querySelector('#chat-wrap iframe')
+
+    const popoutButtonElement = panelToolsElement.querySelector('#popout')
+    if (popoutButtonElement) {
+        popoutButtonElement.addEventListener('click', e => {
+            e.preventDefault()
+
+            popoutWindow = window.open(CHAT_EMBED_URL, '_blank', getPopupOptionsString())
+            popoutWindow.addEventListener('beforeunload', () => {
+                document.body.classList.remove('nochat')
+
+                if (chatIFrameElement) {
+                    chatIFrameElement.src = CHAT_EMBED_URL
+                }
+            })
+            hideChat(chatIFrameElement)
+        })
     }
 
-    panelToolsElement.addEventListener('click', e => {
-        /** @var {Element} triggerElement */
-        const triggerElement = e.target
-
-        if (triggerElement.id === 'popout') {
+    const refreshButtonElement = panelToolsElement.querySelector('#refresh')
+    if (refreshButtonElement) {
+        refreshButtonElement.addEventListener('click', e => {
             e.preventDefault()
 
-            window.open('/embed/chat', '_blank', getPopupOptionsString())
-            document.body.classList.add('nochat')
-            chatPanelElement.remove()
-        } else if (triggerElement.id === 'refresh') {
-            e.preventDefault()
+            if (chatIFrameElement.contentWindow) {
+                chatIFrameElement.contentWindow.location.reload()
+            }
+        })
+    }
 
-            chatWindow.location.reload()
-        } else if (triggerElement.id === 'close') {
+    const closeButtonElement = panelToolsElement.querySelector('#close')
+    if (closeButtonElement) {
+        closeButtonElement.addEventListener('click', e => {
             e.preventDefault()
+            hideChat(chatIFrameElement)
+        })
+    }
 
-            document.body.classList.add('nochat')
-            chatPanelElement.remove()
-        } else if (triggerElement.id === 'swap') {
+    const swapButtonElement = panelToolsElement.querySelector('#swap')
+    if (swapButtonElement) {
+        swapButtonElement.addEventListener('click', e => {
             e.preventDefault()
 
             setOrientation(getOrientation() === '1' ? '0' : '1')
             applyOrientation()
-        }
-    })
+        })
+    }
 }
-
-chatIFrameElement.addEventListener('load', onIframeLoaded)
